bca9a7dbf550dc7d21073060f7350a7e0b107f01
jaeriiimy <riim715@naver.com>
2017-09-25 16:34:46 +0900

    Create mouth.py

---
+++ mouth.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+nose_cascade = cv2.CascadeClassifier('haarcascade_nose.xml')
+mouth_cascade = cv2.CascadeClassifier('haarcascade_mouth.xml')
+
+img = cv2.imread('6.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    nose = nose_cascade.detectMultiScale(roi_gray)
+    mouth = mouth_cascade.detectMultiScale(roi_gray)
+
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+    for (nx,ny,nw,nh) in nose:
+        cv2.rectangle(roi_color,(nx,ny),(nx+nw,ny+nh),(0,0,255),2)
+    for (mx,my,mw,mh) in mouth:
+        cv2.rectangle(roi_color,(mx,my),(mx+mw,my+mh),(255,0,255),2)
+
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
